2f08b51a231a0593802abc951b32138ac964970d
srs-sieam <srssieam@gmail.com>
2023-12-21 07:08:39 +0600

    created react app and installed react router dom

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const Router = createBrowserRouter([
+    {
+        path:"/",
+        element:<h1>Hello world</h1>
+    }
+])
+
+export default Router;
