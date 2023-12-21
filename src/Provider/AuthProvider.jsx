eeeb20be774dcd8c299d6d3f5820aedbefc228b1
srs-sieam <srssieam@gmail.com>
2023-12-21 10:37:20 +0600

    declared all authentication method

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import app from "../../firebase.config";
-import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 
 const AuthContext = createContext();
 const auth = getAuth(app);
@@ -8,7 +8,49 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-    const authInfo = { user, loading }
+
+    const GoogleProvider = new GoogleAuthProvider();
+
+    // sign up
+    const createUser = (email, Password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, Password)
+    }
+
+    // update
+    const updateUserProfile = (name) =>{
+        setLoading(true);
+        updateProfile(auth.currentUser, {displayName: name})
+    }
+
+    // sign out
+    const logOut = ()=>{
+        signOut(auth);
+    }
+
+    // sign in
+    const logIn = (email, Password) => {
+        return signInWithEmailAndPassword(auth, email, Password)
+    }
+
+    // google sign in
+    const googleLogin = () => {
+        return signInWithPopup(auth, GoogleProvider)
+    }
+
+    // observer
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log("current user", currentUser);
+            setLoading(false);
+        })
+        return () => {
+            return unsubscribe();
+        }
+    },[])
+
+    const authInfo = { user, loading, createUser, updateUserProfile, logOut, logIn, googleLogin }
     return (
         <AuthContext.Provider value={ authInfo }>
             {children}
